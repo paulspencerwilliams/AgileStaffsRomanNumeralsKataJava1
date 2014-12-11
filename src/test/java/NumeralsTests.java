9e8d10eccbdc11123624f1564b0a49302220a5b2
Paul Williams <paul@paulswilliams.me.uk>
2014-12-11 19:45:41 +0000

    Refactored

--- src/test/java/NumeralsTests.java
+++ src/test/java/NumeralsTests.java
@@ -5,15 +5,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class NumeralsTests {
 
+    private final NumeralConverter converter = new NumeralConverter();
+
     @Test
     public void testIReturns1() {
-        NumeralConverter converter = new NumeralConverter();
         assertThat(converter.fromRomanNumeral("I"), equalTo((Integer) 1));
     }
 
     @Test
     public void testIIReturns2() {
-        NumeralConverter converter = new NumeralConverter();
         assertThat(converter.fromRomanNumeral("II"), equalTo((Integer)2));
     }
 
